a74978dde655f351db186b5b67aeb5e6b41ae674
Margaret Thomas <magz.thomas@gmail.com>
2017-01-31 14:31:51 +0200

    Stuff

--- controllers/pollController.js
+++ controllers/pollController.js
@@ -36,6 +36,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
 app.controller('cmsController', function($scope, $state){
 	// Some functions to update the state.
 	$scope.goToHome = function(){
+		$scope.poll =  [{pollStatus:"Closed", pollID:"cd5f4e71-0637-4839-93fa-78cd3d598dda", title:"Favourite Vegetable", description:"Poll to determine the most favourite vegetable.", question:"What is your favourite veggie?", answerCount:3, totalVotes:20, endDate:"2017-01-15T16:30:00Z", thanksMessage:"Thanks for participating in the Favourite Vegetable Poll!!", closedMessage:"The poll is now closed", websiteForSharing:"https://www.yfm.co.za/", author: "DJ John"}, {pollStatus:"Open", pollID:"57fc2f6d-82e7-e611-af1f-00dbdf9ddb35", title:"Favourite Fruit", description:"Poll to find out what is the most favourite fruit.", question:"What is your favourite fruit?", answerCount:3, totalVotes:20, endDate:"2017-02-19T16:30:00Z", thanksMessage:"Thanks for participating in the Favourite fruit Poll!!", closedMessage:"The poll is now closed", websiteForSharing:"https://www.yfm.co.za/", author: "DJ John"}, {pollStatus:"Open", pollID:"6a3fc393-82e7-e611-af1f-00dbdf9ddb35", title:"Favourite Genre", question:"What type of music would you like to hear today?", answerCount:4, totalVotes:10, startDate:"2017-01-31T08:00:00Z", endDate:"2017-01-31T17:00:00Z", thanksMessage:"Thanks for your feedback!", closedMessage:"The Genre Poll is now Closed!", websiteForSharing:"https://www.yfm.co.za/", author: "DJ John"}];
 		$state.go("home");
 	};
 	$scope.goToAddPoll = function(){
@@ -43,7 +44,7 @@ app.controller('cmsController', function($scope, $state){
 	}
 	$scope.goToAddAnswers = function(){
 		// Capture new poll details.
-		var newPoll = {pollStatus:"Open", pollID:"3", title:"Favourite Genre", question:"What type of music would you like to hear today?", answerCount:4, totalVotes:10, startDate:"2017-01-31T08:00:00Z", endDate:"2017-01-31T17:00:00Z", thanksMessage:"Thanks for your feedback!", closedMessage:"The Genre Poll is now Closed!", websiteForSharing:"https://www.ecr.co.za/"};
+		var newPoll = {pollStatus:"Open", pollID:"6a3fc393-82e7-e611-af1f-00dbdf9ddb35", title:"Favourite Genre", question:"What type of music would you like to hear today?", answerCount:4, totalVotes:10, startDate:"2017-01-31T08:00:00Z", endDate:"2017-01-31T17:00:00Z", thanksMessage:"Thanks for your feedback!", closedMessage:"The Genre Poll is now Closed!", websiteForSharing:"https://www.yfm.co.za/", author: "DJ John"};
 		localStorage.setItem("poll", JSON.stringify(newPoll));
 		$state.go("addAnswers");
 	}
@@ -62,7 +63,7 @@ app.controller('cmsController', function($scope, $state){
 		$state.go("results");
 	}
 	// Dummy content for now. Here is where the the API request will be made.
-	var data = [{pollStatus:"Closed", pollID:"1", title:"Favourite Vegetable", description:"Poll to determine the most favourite vegetable.", question:"What is your favourite veggie?", answerCount:3, totalVotes:20, endDate:"2017-01-15T16:30:00Z", thanksMessage:"Thanks for participating in the Favourite Vegetable Poll!!", closedMessage:"The poll is now closed", websiteForSharing:"www.google.com"}, {pollStatus:"Open", pollID:"2", title:"Favourite Fruit", description:"Poll to find out what is the most favourite fruit.", question:"What is your favourite fruit?", answerCount:3, totalVotes:20, endDate:"2017-02-19T16:30:00Z", thanksMessage:"Thanks for participating in the Favourite fruit Poll!!", closedMessage:"The poll is now closed", websiteForSharing:"www.google.com"}];
+	var data = [{pollStatus:"Closed", pollID:"cd5f4e71-0637-4839-93fa-78cd3d598dda", title:"Favourite Vegetable", description:"Poll to determine the most favourite vegetable.", question:"What is your favourite veggie?", answerCount:3, totalVotes:20, endDate:"2017-01-15T16:30:00Z", thanksMessage:"Thanks for participating in the Favourite Vegetable Poll!!", closedMessage:"The poll is now closed", websiteForSharing:"www.google.com", author: "DJ John"}, {pollStatus:"Open", pollID:"57fc2f6d-82e7-e611-af1f-00dbdf9ddb35", title:"Favourite Fruit", description:"Poll to find out what is the most favourite fruit.", question:"What is your favourite fruit?", answerCount:3, totalVotes:20, endDate:"2017-02-19T16:30:00Z", thanksMessage:"Thanks for participating in the Favourite fruit Poll!!", closedMessage:"The poll is now closed", websiteForSharing:"https://www.yfm.co.za", author: "DJ John"}];
 	
 	$scope.poll = data;
 	
@@ -80,6 +81,11 @@ app.controller('cmsController', function($scope, $state){
 			$scope.ansVote.push(singleAnswer[1]);
 		}
 		$scope.chosenAnswer = localStorage.getItem("chosenAnswer");
+	}
+	$scope.loadResults = function(){
+		$scope.loadWidgetInfo();
+		$scope.platforms = [["iOS",5], ["Android",3], ["Web", 2]];
+		$scope.locations = [["Durban",4], ["Cape Town",3], ["Johannesburg", 3]];
 	}
 	$scope.formatNumber = function(votes) {
 		return Math.round(votes); 
