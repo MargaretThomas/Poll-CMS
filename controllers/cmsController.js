adf9d0444d258813079fee0a530e30ca79a32e1d
Margaret Thomas <magz.thomas@gmail.com>
2017-02-15 13:54:26 +0200

    Delete option added. Working on close poll option

--- controllers/cmsController.js
+++ controllers/cmsController.js
@@ -1,4 +1,4 @@
-var app = angular.module("pollApp", ["ui.router", "ngMaterial"])
+var app = angular.module("pollApp", ["ui.router", "ngMaterial", "ngMessages"])
 app.factory('myFactory', ['$http',
         function($http) {
 			// Get all polls for the tenant.
@@ -12,15 +12,32 @@ app.factory('myFactory', ['$http',
 				},
 				}).then(function successCallback(response) {
 					// This callback will be called asynchronously when the response is available.
-					callback(response.data);
+					callback(response);
 				}, function errorCallback(response) {
 					// Called asynchronously if an error occurs or server returns response with an error status.
-					alert("Could not load all the polls.");
+					callback(response);
 				});
 			}
+			// Getting the total votes
+			var getVoteCounts = function(callback, pollID) {
+				$http({
+				method: 'GET',
+				url: 'http://pollapi.azurewebsites.net/98b6b223-6849-4c3b-8c50-1f42f26946ed/api/AnsResult/Answers/'+pollID,
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*'
+				}
+				}).then(function successCallback(response) {
+					// This callback will be called asynchronously when the response is available.
+					callback(response);
+				}, function errorCallback(response) {
+					// Called asynchronously if an error occurs or server returns response with an error status.
+					callback(response);
+				});
+			};
 			// Creates a new poll.
-			var newPoll = function(objPoll){
-				var request = {
+			var newPoll = function(callback, objPoll){
+				$http({
 					method: 'POST',
 					url: 'http:/pollapi.azurewebsites.net/98b6b223-6849-4c3b-8c50-1f42f26946ed/api/polls',
 					headers: {
@@ -29,12 +46,12 @@ app.factory('myFactory', ['$http',
 					'Access-Control-Allow-Origin': '*'
 					},
 					data: JSON.stringify(objPoll)
-				}
-				$http(request).then(function(){
-					alert("POSTed");
-				}, function()
-				{
-					alert("not POSTed");
+				}).then(function successCallback(response) {
+					// This callback will be called asynchronously when the response is available.
+					callback(response);
+				}, function errorCallback(response) {
+					// Called asynchronously if an error occurs or server returns response with an error status.
+					callback(response);
 				});
 			}
 			// Get the total votes per answer.
@@ -54,10 +71,88 @@ app.factory('myFactory', ['$http',
 					callback(response);
 				});
 			}
+			// Get the last poll. Need the guid for the widget.
+			var getLastPoll = function(callback){
+				$http({
+				method: 'GET',
+				url: 'http://pollapi.azurewebsites.net/98b6b223-6849-4c3b-8c50-1f42f26946ed/api/polls/last',
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*'
+				},
+				}).then(function successCallback(response) {
+					// This callback will be called asynchronously when the response is available.
+					callback(response);
+				}, function errorCallback(response) {
+					// Called asynchronously if an error occurs or server returns response with an error status.
+					callback(response);
+				});
+			}
+			// Get the poll details for the poll that needs to be updated.
+			var getSpecific = function(callback, pollID){
+				$http({
+				method: 'GET',
+				url: 'http://pollapi.azurewebsites.net/98b6b223-6849-4c3b-8c50-1f42f26946ed/api/polls/' +pollID,
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*'
+				},
+				}).then(function successCallback(response) {
+					// This callback will be called asynchronously when the response is available.
+					callback(response);
+				}, function errorCallback(response) {
+					// Called asynchronously if an error occurs or server returns response with an error status.
+					callback(response);
+				});
+			}
+			// Close a poll.
+			var closePoll = function(callback, pollID, obj){
+				$http({
+				method: 'PUT',
+				url: 'http://pollapi.azurewebsites.net/98b6b223-6849-4c3b-8c50-1f42f26946ed/api/polls/'+pollID,
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*',
+					'mimeType': 'json'
+				},
+				data: obj
+				}).then(function successCallback(response) {
+					// This callback will be called asynchronously when the response is available.
+					callback(response);
+				}, function errorCallback(response) {
+					// Called asynchronously if an error occurs or server returns response with an error status.
+					callback(response);
+					toastr.info(response.status);
+				});
+			}
+			// Delete a poll.
+			var deletePoll = function(callback, pollID, obj){
+				$http({
+				method: 'DELETE',
+				url: 'http://pollapi.azurewebsites.net/98b6b223-6849-4c3b-8c50-1f42f26946ed/api/polls/DELETE/'+pollID,
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*'
+				},
+				data: JSON.stringify(obj)
+				}).then(function successCallback(response) {
+					// This callback will be called asynchronously when the response is available.
+					callback(response);
+				}, function errorCallback(response) {
+					// Called asynchronously if an error occurs or server returns response with an error status.
+					callback(response);
+					toastr.info(response.status);
+				});
+			}
             return {
 				funcAllPolls: getAllPolls,
+				funcVoteCounts: getVoteCounts,
 				funcNewPoll: newPoll,
-				funcMetaData: getMetaData
+				funcMetaData: getMetaData,
+				funcLastPoll: getLastPoll,
+				funcSpecificPoll: getSpecific,
+				funcClosePoll: closePoll,
+				funcDeletePoll: deletePoll
             }
         }
 ]);
@@ -120,89 +215,238 @@ app.config(function($stateProvider, $urlRouterProvider) {
 	});
 });
 app.controller('cmsController', function($scope, $state, myFactory){
-	// Some functions to update the state.
+	// Set the properties of the Toastr messages.
+	toastr.options = {
+		"closeButton": true,
+		"newestOnTop": true,
+		"progressBar": false,
+		"positionClass": "toast-top-left"
+	};
+	// HOME.
 	$scope.goToHome = function(){
+		window.localStorage.clear();
 		$state.go("home");
 	};
+	// Store all the polls for this tenant. To be displayed in the home screen.
+	$scope.gettingAllPolls = function(){
+		// Clear storage.
+		window.localStorage.clear();
+		$scope.polls = [];
+		$scope.totalVotes = [];
+		myFactory.funcAllPolls(function(response){
+			if(response.status >= 200 && response.status < 300){
+				toastr.info("Retrieving all your polls");
+				// Success callback.
+				var data = response.data;
+				// Gett all the polls.
+				for(var index=0;index<data.length;index++){
+					data[index].start_date = (new Date(data[index].start_date)).toDateString();
+					data[index].end_date = (new Date(data[index].end_date)).toDateString();
+					$scope.polls.push(data[index]);
+					
+					// Get the Vote Counts for each answer of each poll.
+					var pollID = data[index].poll_guid;
+					myFactory.funcVoteCounts(function(response){
+						if(response.status >= 200 && response.status < 300){
+							var data = response.data;
+							$scope.answersCount = [];
+							$scope.answersCount.push(data.ans_vote_1);
+							$scope.answersCount.push(data.ans_vote_2);
+							$scope.answersCount.push(data.ans_vote_3);
+							$scope.answersCount.push(data.ans_vote_4);
+							var total = 0;
+							for(var indexAns=0;indexAns<$scope.answersCount.length;indexAns++){
+								total = total + $scope.answersCount[indexAns]; 
+							}
+							// Store the total votes for all the polls.
+							localStorage.setItem("totalVotes", JSON.stringify($scope.totalVotes));
+						}else if(response.status == 404){
+							// There are no answers as yet. 404 - not found.
+							$scope.totalVotes.push(0);
+						}					
+					}, pollID);
+				}
+			}else{
+				// Failed callback.
+				toastr.error("Unable to receive all your polls. Please refresh this page.");
+			}
+		});
+		
+	}
+	
+	// ADD POLL DETAILS.
 	$scope.goToAddPoll = function(){
+		$state.go("addPoll");
+	}
+	$scope.loadPollDetails = function(){
+		$scope.poll = JSON.parse(localStorage.getItem("poll"));
+		if($scope.poll != null){
+			$scope.poll.startDate = new Date($scope.poll.startDate);
+			$scope.poll.startTime = new Date($scope.poll.startTime);
+			$scope.poll.endDate = new Date($scope.poll.endDate);
+			$scope.poll.endTime = new Date($scope.poll.endTime);
+		}
+	}
+	$scope.backToAddPoll = function(){
+		$scope.loadPollDetails();
 		$state.go("addPoll");
 	}
 	$scope.goToAddAnswers = function(){	
 		// Validation:
-		var title = $scope.poll.title;
-		var description = $scope.poll.description;
-		var question = $scope.poll.question;
-		var thanksMessage = $scope.poll.thanksMessage;
-		var closedMessage = $scope.poll.closedMessage;
-		var notifyText = $scope.poll.notifyText;
-		var sharingURL = $scope.poll.sharingURL;
-		var createdBy = $scope.poll.createdBy;
-		var startDate = $scope.poll.startDate;
-		var startTime = $scope.poll.startTime;
-		var endDate = $scope.poll.endDate;
-		var endTime = $scope.poll.endTime;
-		
-		localStorage.setItem("poll", JSON.stringify($scope.poll));
+		if(!($scope.poll == undefined || $scope.poll.title == undefined || $scope.poll.description == undefined || $scope.poll.question == undefined || $scope.poll.closedMessage == undefined || $scope.poll.notifyText == undefined || $scope.poll.sharingURL == undefined || $scope.poll.author == undefined || $scope.poll.startDate == undefined || $scope.poll.startTime == undefined || $scope.poll.endDate == undefined || $scope.poll.endTime == undefined)){
+			toastr.info("Data entered is valid");
+			localStorage.setItem("poll", JSON.stringify($scope.poll));
+			$state.go("addAnswers");		
+		}else{
+			if($scope.poll != undefined){
+				if($scope.poll.startDate == undefined){
+					toastr.error("Please enter the start date of the poll.");
+				}
+			}	
+		}
+	}
+	// ADD ANSWERS FOR A POLL.
+	$scope.loadAnswers = function(){
+		$scope.answers = JSON.parse(localStorage.getItem("answers"));
+	}
+	$scope.backToAnswers = function(){
+		$scope.loadAnswers();
 		$state.go("addAnswers");
 	}
 	$scope.goToPreview = function(){
 		// Capture the answers.
-		var chosenAnswer = "Jazz";
-		localStorage.setItem("answers", JSON.stringify($scope.answers));
-		localStorage.setItem("chosenAnswer", chosenAnswer);
-		$state.go("preview");
+		if($scope.answers == undefined || $scope.answers.option1 == undefined || $scope.answers.option2 == undefined){
+			toastr.error("Please enter valid data for the options of this poll.");
+		}else{
+			toastr.info("Data entered is valid");
+			var chosenAnswer = "";
+			localStorage.setItem("answers", JSON.stringify($scope.answers));
+			localStorage.setItem("chosenAnswer", chosenAnswer);
+			$state.go("preview");
+		}
 	}
+	// PREVIEW A POLL.
 	$scope.goToPublish = function(){
-		
+		// Get data ready for publishing
 		$scope.poll = JSON.parse(localStorage.getItem("poll"));
 		$scope.answers = JSON.parse(localStorage.getItem("answers"));
 		
+		// Set the dates up. From the date time picker and time input.
 		var hours = new Date($scope.poll.startTime);
 		var oldStart = new Date($scope.poll.startDate);
 		var starting = new Date(oldStart.setHours(hours.getHours()));
+		starting = new Date(oldStart.setMinutes(hours.getMinutes()));
 		
 		var hours2 = new Date($scope.poll.endTime);
 		var oldEnd = new Date($scope.poll.endDate);
 		var ending = new Date(oldEnd.setHours(hours2.getHours()));
+		ending = new Date(oldEnd.setMinutes(hours2.getMinutes()));
 		
-		var newPoll = {"poll_title": $scope.poll.title,"poll_description": $scope.poll.description,"status": 1,"question": $scope.poll.question,"ans_count": 4,"answer": {"answer_id": 1,"answer": $scope.answers.option1},"answer2": {"answer_id": 2,"answer": $scope.answers.option2},"answer3": {"answer_id": 3,"answer": $scope.answers.option3},"answer4": {"answer_id": 4,"answer": $scope.answers.option4},"start_date": starting,"end_date": ending,"thanks_message": $scope.poll.thanksMessage,"closed_message": $scope.poll.closedMessage,  "notification_text": $scope.poll.notifyText,"share_url": $scope.poll.sharingURL,"author": $scope.poll.author};
+		// Get the time offset.
+		var dateNow = new Date();
+		var difference = dateNow.getTimezoneOffset();
+		if(difference > 0){
+			var absolute = Math.abs(difference);
+			// Add on the time.
+			var h = absolute/60;
+			var m = absolute%60;
+			// Start Date.
+			starting.setHours(starting.getHours() + h);
+			starting.setMinutes(starting.getMinutes() + m);
+			// End Date.
+			ending.setHours(starting.getHours() + h);
+			ending.setMinutes(starting.getMinutes() + m);
+		} else if(difference < 0){
+			var absolute = Math.abs(difference);
+			// Add on the time.
+			var h = absolute/60;
+			var m = absolute%60;
+			// Start Date.
+			starting.setHours(starting.getHours() - h);
+			starting.setMinutes(starting.getMinutes() - m);
+			// End Date.
+			ending.setHours(starting.getHours() - h);
+			ending.setMinutes(starting.getMinutes() - m);
+		}
+		// Setting up the object, depends on how many answers there are.
+		var ans_count = 0;
+		var newPoll = {};
+		if($scope.answers.option3 == undefined && $scope.answers.option4 == undefined){
+			ans_count = 2;
+			newPoll = {"poll_title": $scope.poll.title,"poll_description": $scope.poll.description,"status": 1,"question": $scope.poll.question,"ans_count": ans_count,"answer": {"answer_id": 1,"answer": $scope.answers.option1},"answer2": {"answer_id": 2,"answer": $scope.answers.option2},"answer3": {"answer_id": 0,"answer": ""},"answer4": {"answer_id": 0,"answer": ""},"start_date": starting,"end_date": ending,"thanks_message": $scope.poll.thanksMessage,"closed_message": $scope.poll.closedMessage,  "notification_text": $scope.poll.notifyText,"share_url": $scope.poll.sharingURL,"author": $scope.poll.author};
+			console.log("2");
+		}else if($scope.answers.option3 == undefined && $scope.answers.option4 != undefined){
+			ans_count = 3;
+			newPoll = {"poll_title": $scope.poll.title,"poll_description": $scope.poll.description,"status": 1,"question": $scope.poll.question,"ans_count": ans_count,"answer": {"answer_id": 1,"answer": $scope.answers.option1},"answer2": {"answer_id": 2,"answer": $scope.answers.option2},"answer3": {"answer_id": 3,"answer": $scope.answers.option4},"answer4": {"answer_id": 0},"start_date": starting,"end_date": ending,"thanks_message": $scope.poll.thanksMessage,"closed_message": $scope.poll.closedMessage, "notification_text": $scope.poll.notifyText,"share_url": $scope.poll.sharingURL,"author": $scope.poll.author};
+			console.log("3..");
+		}else if($scope.answers.option3 != undefined && $scope.answers.option4 == undefined){
+			ans_count = 3;
+			newPoll = {"poll_title": $scope.poll.title,"poll_description": $scope.poll.description,"status": 1,"question": $scope.poll.question,"ans_count": ans_count,"answer": {"answer_id": 1,"answer": $scope.answers.option1},"answer2": {"answer_id": 2,"answer": $scope.answers.option2},"answer3": {"answer_id": 3,"answer": $scope.answers.option3},"answer4": {"answer_id": 0}, "start_date": starting,"end_date": ending,"thanks_message": $scope.poll.thanksMessage,"closed_message": $scope.poll.closedMessage,  "notification_text": $scope.answers.notifyText,"share_url": $scope.poll.sharingURL,"author": $scope.poll.author};
+			console.log("3.+");
+		}else{
+			ans_count = 4;
+			newPoll = {"poll_title": $scope.poll.title,"poll_description": $scope.poll.description,"status": 1,"question": $scope.poll.question,"ans_count": ans_count,"answer": {"answer_id": 1,"answer": $scope.answers.option1},"answer2": {"answer_id": 2,"answer": $scope.answers.option2},"answer3": {"answer_id": 3,"answer": $scope.answers.option3},"answer4": {"answer_id": 4,"answer": $scope.answers.option4},"start_date": starting,"end_date": ending,"thanks_message": $scope.poll.thanksMessage,"closed_message": $scope.poll.closedMessage,  "notification_text": $scope.poll.notifyText,"share_url": $scope.poll.sharingURL,"author": $scope.poll.author};
+			console.log("4");
+		}
 		
-		myFactory.funcNewPoll(newPoll);
-		$state.go("publish");
-	}
-	$scope.goToResults = function(){
-		$state.go("results");
-	}
-	// Store all the polls for this tenant. To be displayed in the home screen.
-	$scope.gettingAllPolls = function(){
-		$scope.polls = [];
-		myFactory.funcAllPolls(function(data){
-			for(var index=0;index<data.length;index++){
-				$scope.polls.push(data[index]);
+		myFactory.funcNewPoll(function(response){
+			if(response.status >= 200 && response.status < 300){
+				// Get the last poll.
+				myFactory.funcLastPoll(function(response){
+					if(response.status >= 200 && response.status < 300){
+						var data = response.data;
+						var latestGuid = data.poll_guid;
+						$scope.embeddableCode = "<iframe src='http://widget.margaret.dx.am/index.html#/firstState/"+latestGuid+"' width='400' height='500' style='background-color: #FFF;font-family: Lato, sans-serif;'></iframe>";
+						console.log($scope.embeddableCode);
+						localStorage.setItem("embeddableCode", $scope.embeddableCode);
+					} else{
+						toastr.error("Unable to receive the new created poll. Pleas refresh this page.")
+					}
+				});
+				toastr.success($scope.poll.title + " added!");
+				$state.go("publish");
+			}else{
+				toastr.error("Poll did not publish. Please try again.")
 			}
-		});
+		},newPoll);
 	}
-		
 	$scope.loadWidgetInfo = function(){
-		/*
 		// Load stuff from dummy API.
 		$scope.ans = [];
 		$scope.ansVote = [];
+		$scope.perc = [0,0,0,0];
 		var index = 0;
 		$scope.newPoll = JSON.parse(localStorage.getItem("poll"));
 		var answers = JSON.parse(localStorage.getItem("answers"));
 		$scope.answers = answers;
-		for(var item in answers){
-			var singleAnswer = answers[item];
-			$scope.ans.push(singleAnswer[0]);
-			$scope.ansVote.push(singleAnswer[1]);
+		if($scope.newPoll == undefined || answers == undefined){
+			toastr.warning("Please make sure to add the poll details and the various answers before previewing.");
+		}else{
+			for(var item in answers){				
+				$scope.ans.push(answers[item]);
+				$scope.ansVote.push(0);
+			}
+			$scope.chosenAnswer = localStorage.getItem("chosenAnswer");
 		}
-		$scope.chosenAnswer = localStorage.getItem("chosenAnswer");
+				
+	}
+	// PUBLISH A POLL.
+	$scope.getNewlyCreatedPoll = function(){
+		// Show full page LoadingOverlay
+		$.LoadingOverlay("show");
 		
-		*/
+		// Hide it after 3 seconds
+		setTimeout(function(){
+			$scope.embeddableCode = localStorage.getItem("embeddableCode");
+			$.LoadingOverlay("hide");
+		}, 3000);
 	}
 	$scope.loadDefault = function(){
 		$state.go("voteCount");
+	}
+	// VIEW RESULTS.
+	$scope.goToResults = function(){
+		$state.go("results");
 	}
 	$scope.loadVoteCount = function(){
 		var resultGUID = localStorage.getItem("resultGUID");
@@ -257,4 +501,6 @@ app.controller('cmsController', function($scope, $state, myFactory){
 			}
 		}, metaEndpoint);
 	}
+	// DETELE A POLL.
+	
 });
--- directives/pollDirectives.js
+++ directives/pollDirectives.js
@@ -3,9 +3,13 @@ app.directive("singlePoll", function() {
     return {
 		restrict: "E",
 		scope:{
-			poll: "=" // Single Poll info.
+			poll: "=",
+			total_votes: "="
 		},
-        templateUrl: "templates/poll-template.html"
+        templateUrl: "templates/poll-template.html",
+		link: function(scope, elem, attrs){
+			console.log(scope.total_votes);
+		}
     };
 });
 // Go to the results page
@@ -21,6 +25,78 @@ app.directive("viewResults", function($state) {
 				console.log(scope.id);
 				localStorage.setItem("resultGUID", scope.id);
 				$state.go("results");
+			});
+		}
+    };
+});
+// Closed a poll.
+app.directive("closePoll", function(myFactory){
+	return{
+		restrict: "E",
+		scope:{
+			id: "="
+		},
+		templateUrl: "templates/closed-poll.html",
+		link: function(scope, elem, attrs){
+			elem.bind('click', function(){
+				var poll_id = scope.id;
+				var confirmClose = confirm("Are you sure that you want to close this poll?");
+				if(confirmClose){
+					// Get the poll details for the specific poll that we want to close.
+					myFactory.funcSpecificPoll(function(response){
+					if(response.status >= 200 && response.status < 300){
+						// Successful Callback.
+						// Get the data.
+						var pollDetails = response.data;
+						// Set the status = 3 : closes the poll.
+						pollDetails.status = 3;
+						var obj = {
+							id: poll_id,
+							polls: JSON.stringify(pollDetails),
+							tennantID: "98b6b223-6849-4c3b-8c50-1f42f26946ed"
+						};
+						// Execute the PUT.
+						myFactory.funcClosePoll(function(response){
+							if(response.status >= 200 && response.status < 300){
+								toastr.success(pollDetails.poll_title+" closed!");
+							}
+						}, poll_id, obj);
+					} else{
+						toastr.error("Unable to closed the poll. Please try again.");
+					}
+				}, poll_id);
+				}else{
+					console.log("no");
+				}
+			});
+		}
+	};
+});
+app.directive("deletePoll", function(myFactory, $state) {
+    return {
+		restrict: "E",
+		scope:{
+			id: "="
+		},
+        templateUrl: "templates/delete-poll.html",
+		link: function(scope, elem, attrs){
+			elem.bind('click', function(){
+				var confirmDelete = confirm("Are you sure that you want to delete this poll?");
+				if(confirmDelete){
+					var poll_id = scope.id;
+					console.log(poll_id);
+					var obj = {
+						id: poll_id,
+						tennantID: "98b6b223-6849-4c3b-8c50-1f42f26946ed"
+					};
+					// Execute the DELETE.
+					myFactory.funcDeletePoll(function(response){
+						if(response.status >= 200 && response.status < 300){
+							toastr.success("Poll is deleted!");
+							$state.go('home', {}, { reload: true });
+						}
+					}, poll_id, obj);
+				}
 			});
 		}
     };
